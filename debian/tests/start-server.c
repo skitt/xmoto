c04a481703150f31012558b4655ac0b8c5390e8c
Stephen Kitt <steve@sk2.org>
2022-04-15 21:33:01 +0200

    Rework the autopkgtest to make it more reliable
    Closes: #1006815

--- debian/tests/start-server.c
+++ debian/tests/start-server.c
@@ -9,6 +9,7 @@
 #include <stdnoreturn.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <time.h>
 #include <unistd.h>
 extern char **environ;
 
@@ -18,12 +19,23 @@ static noreturn void xmoto_died(int s) {
 	_Exit(EXIT_FAILURE);
 }
 
+static int cleanup(pid_t xmoto, int sock, int status) {
+	if (kill(xmoto, SIGTERM) == -1) {
+		perror("Failed to terminate X-Moto");
+	}
+	if (sock >= 0 && close(sock) == -1) {
+		perror("Failed to close socket");
+	}
+	exit(status);
+}
+
 int main(void) {
 	struct sigaction act = { .sa_handler = xmoto_died };
 	if(sigemptyset(&act.sa_mask) == -1) {
 		perror("Failed to empty signal set");
 		exit(EXIT_FAILURE);
 	}
+	sigaction(SIGCHLD, &act, NULL);
 
 	pid_t xmoto;
 	int k = posix_spawnp(&xmoto, "xmoto", NULL, NULL, (char*[]){"xmoto", "--server", NULL}, environ);
@@ -32,69 +44,55 @@ int main(void) {
 		exit(EXIT_FAILURE);
 	}
 
+	time_t start = time(NULL);
 	/* Try IPv6 first, then IPv4. */
-	sa_family_t fam = AF_INET6;
-	int sock;
-startconn:
-	sock = socket(fam, SOCK_STREAM, IPPROTO_TCP);
-	if(sock == -1) {
-		perror("Failed to create socket");
-		if(kill(xmoto, SIGTERM) == -1) {
-			perror("Failed to terminate X-Moto");
+	sa_family_t families[] = { AF_INET6, AF_INET };
+
+	while (1) {
+		if (time(NULL) - start > 60) {
+			perror("Timed out waiting for X-Moto");
+			exit(EXIT_FAILURE);
 		}
-		exit(EXIT_FAILURE);
+
+		for (int i = 0; i < 2; i++) {
+			sa_family_t fam = families[i];
+			int sock;
+			sock = socket(fam, SOCK_STREAM, IPPROTO_TCP);
+			if (sock == -1) {
+				perror("Failed to create socket");
+				cleanup(xmoto, sock, EXIT_FAILURE);
+			}
+
+			union {
+				struct sockaddr sa;
+				struct sockaddr_in in;
+				struct sockaddr_in6 in6;
+			} addr = {0};
+			switch(addr.sa.sa_family = fam) {
+			case AF_INET:
+				addr.in.sin_port = htons(XMOTO_PORT);
+				addr.in.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+				break;
+			case AF_INET6:
+				addr.in6.sin6_port = htons(XMOTO_PORT);
+				addr.in6.sin6_addr = in6addr_loopback;
+				break;
+			default:
+				cleanup(xmoto, sock, EXIT_FAILURE);
+			}
+
+			if (connect(sock, &addr.sa, sizeof(addr)) == 0) {
+				/* Success! */
+				cleanup(xmoto, sock, EXIT_SUCCESS);
+			}
+			if (close(sock) == -1) {
+				perror("Failed to close socket");
+				cleanup(xmoto, -1, EXIT_FAILURE);
+			}
+		}
+
+		sleep(1);
 	}
 
-	union {
-		struct sockaddr sa;
-		struct sockaddr_in in;
-		struct sockaddr_in6 in6;
-	} addr = {0};
-	switch(addr.sa.sa_family = fam) {
-	case AF_INET:
-		addr.in.sin_port = htons(XMOTO_PORT);
-		addr.in.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-		break;
-	case AF_INET6:
-		addr.in6.sin6_port = htons(XMOTO_PORT);
-		addr.in6.sin6_addr = in6addr_loopback;
-		break;
-	default:
-		if(kill(xmoto, SIGTERM) == -1) {
-			perror("Failed to terminate X-Moto");
-		}
-		if(close(sock) == -1) {
-			perror("Failed to close socket");
-		}
-		abort();
-	}
-
-	sleep(3U); /* X-Moto needs a chance to start */
-	if(connect(sock, &addr.sa, sizeof(addr)) == -1) {
-		if(fam == AF_INET6) {
-			/* Try again with IPv4. */
-			fam = AF_INET;
-			goto startconn;
-		}
-		perror("Failed to connect to X-Moto");
-		if(kill(xmoto, SIGTERM) == -1) {
-			perror("Failed to terminate X-Moto");
-		}
-		if(close(sock) == -1) {
-			perror("Failed to close socket");
-		}
-		exit(EXIT_FAILURE);
-	}
-
-	if(close(sock) == -1) {
-		perror("Failed to close socket");
-		if(kill(xmoto, SIGTERM) == -1) {
-			perror("Failed to terminate X-Moto");
-		}
-		exit(EXIT_FAILURE);
-	}
-	if(kill(xmoto, SIGTERM) == -1) {
-		perror("Failed to terminate X-Moto");
-		exit(EXIT_FAILURE);
-	}
+	cleanup(xmoto, -1, EXIT_FAILURE);
 }
